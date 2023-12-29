679a32fa34e56b462afd5a0da908d7ca959bed78
Camillo Facello <hello@camillof.dev>
2023-12-29 17:11:01 -0300

    Add typing sound

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const text = "camillof.dev";
 const typingText = document.getElementById("typing-text");
+const typingSound = document.getElementById("typing-sound");
 let index = 0;
 
 function typeText() {
-    if (index < text.length) {
-        typingText.innerHTML += text.charAt(index);
-        index++;
-        setTimeout(typeText, 150);
-    }
+  if (index === 0) {
+    typingSound.play();
+  }
+
+  if (index < text.length) {
+    typingText.innerHTML += text.charAt(index);
+    index++;
+    setTimeout(typeText, 150);
+  } else {
+    typingSound.pause();
+    typingSound.currentTime = 0;
+  }
 }
 
 window.onload = typeText;
