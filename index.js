a820f774dad18ab01f2a80a62e052eb886961b33
Camillo Facello <hello@camillof.dev>
2024-03-25 13:13:10 -0300

    Add shuffling effect while writing

--- index.js
+++ index.js
@@ -3,19 +3,47 @@ const typingText = document.getElementById("typing-text");
 const typingSound = document.getElementById("typing-sound");
 let index = 0;
 
-function typeText() {
-  if (index === 0) {
-    typingSound.play();
+async function typeText() {
+  typingSound.play();
+
+  while (index < text.length) {
+    await shuffleChar(index, text.charAt(index));
+    index++;
   }
 
-  if (index < text.length) {
-    typingText.innerHTML += text.charAt(index);
-    index++;
-    setTimeout(typeText, 150);
-  } else {
-    typingSound.pause();
-    typingSound.currentTime = 0;
-  }
+  typingSound.pause();
+  typingSound.currentTime = 0;
+}
+
+function shuffleChar(position, actualChar) {
+  return new Promise((resolve) => {
+    const shuffleCount = 3;
+    let count = 0;
+
+    const shuffle = () => {
+      if (count < shuffleCount) {
+        const characters =
+          "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789?!@#$%^&*()_+-=[]{}|;:,.<>/?\\";
+        let randomChar = characters.charAt(
+          Math.floor(Math.random() * characters.length)
+        );
+        typingText.textContent =
+          typingText.textContent.substring(0, position) +
+          randomChar +
+          typingText.textContent.substring(position + 1);
+        count++;
+        setTimeout(shuffle, 150);
+      } else {
+        typingText.textContent =
+          typingText.textContent.substring(0, position) +
+          actualChar +
+          typingText.textContent.substring(position + 1);
+        resolve();
+      }
+    };
+
+    shuffle();
+  });
 }
 
 window.onload = typeText;
