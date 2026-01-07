c04f28006825668e925e4673060772ae6f8c3db4
Camillo Facello <hello@camillof.dev>
2026-01-07 10:52:52 -0300

    Replace dots by some skill icons

--- particles.js
+++ particles.js
@@ -8,13 +8,62 @@ const particles = [];
 const particleCount = Math.floor((canvas.width * canvas.height) / 10000);
 const maxDistance = 100;
 
+const techIcons = [
+  'assets/tech-icons/ruby.svg',
+  'assets/tech-icons/rails.svg',
+  'assets/tech-icons/react.svg',
+  'assets/tech-icons/javascript.svg',
+  'assets/tech-icons/typescript.svg',
+  'assets/tech-icons/html5.svg',
+  'assets/tech-icons/css3.svg',
+  'assets/tech-icons/git.svg',
+  'assets/tech-icons/docker.svg'
+];
+
+const loadedImages = [];
+let imagesLoaded = 0;
+
+function preloadImages(callback) {
+  if (techIcons.length === 0) {
+    callback();
+    return;
+  }
+
+  techIcons.forEach((src, index) => {
+    const img = new Image();
+    img.onload = () => {
+      imagesLoaded++;
+      if (imagesLoaded === techIcons.length) {
+        callback();
+      }
+    };
+    img.onerror = () => {
+      console.warn(`Failed to load icon: ${src}`);
+      imagesLoaded++;
+      if (imagesLoaded === techIcons.length) {
+        callback();
+      }
+    };
+    img.src = src;
+    loadedImages[index] = img;
+  });
+}
+
 class Particle {
-  constructor(x, y) {
+  constructor(x, y, forceDot = false) {
     this.x = x || Math.random() * canvas.width;
     this.y = y || Math.random() * canvas.height;
     this.vx = Math.random() - 0.5;
     this.vy = Math.random() - 0.5;
-    this.radius = 2;
+
+    this.isIcon = !forceDot && Math.random() < 0.3 && loadedImages.length > 0;
+
+    if (this.isIcon) {
+      this.iconIndex = Math.floor(Math.random() * loadedImages.length);
+      this.size = 20;
+    } else {
+      this.radius = 2;
+    }
   }
 
   update() {
@@ -26,11 +75,24 @@ class Particle {
   }
 
   draw() {
-    ctx.beginPath();
-    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
-    ctx.fillStyle = "#fff";
-    ctx.fill();
-    ctx.closePath();
+    if (this.isIcon && loadedImages[this.iconIndex]) {
+      ctx.save();
+      ctx.filter = 'brightness(0) invert(1)';
+      ctx.drawImage(
+        loadedImages[this.iconIndex],
+        this.x - this.size / 2,
+        this.y - this.size / 2,
+        this.size,
+        this.size
+      );
+      ctx.restore();
+    } else {
+      ctx.beginPath();
+      ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+      ctx.fillStyle = "#fff";
+      ctx.fill();
+      ctx.closePath();
+    }
   }
 }
 
@@ -78,17 +140,20 @@ window.addEventListener("resize", () => {
 });
 
 window.addEventListener("click", (event) => {
-  particles.push(new Particle(event.clientX, event.clientY));
+  particles.push(new Particle(event.clientX, event.clientY, true));
 });
 
 window.addEventListener("touchend", (event) => {
   particles.push(
     new Particle(
       event.changedTouches[0].clientX,
-      event.changedTouches[0].clientY
+      event.changedTouches[0].clientY,
+      true
     )
   );
 });
 
-initParticles();
-animate();
+preloadImages(() => {
+  initParticles();
+  animate();
+});
