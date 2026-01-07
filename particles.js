e3169f9453b42be0800ba28d49d3789d26d104d0
Camillo Facello <hello@camillof.dev>
2026-01-07 12:00:09 -0300

    Fix icons tinting issue in Safari

--- particles.js
+++ particles.js
@@ -7,6 +7,7 @@ canvas.height = window.innerHeight;
 const particles = [];
 const particleCount = Math.floor((canvas.width * canvas.height) / 10000);
 const maxDistance = 100;
+const maxDistanceSquared = maxDistance * maxDistance;
 
 const techIcons = [
   'assets/tech-icons/ruby.svg',
@@ -21,7 +22,23 @@ const techIcons = [
 ];
 
 const loadedImages = [];
+const whiteTintedImages = [];
 let imagesLoaded = 0;
+
+function createWhiteTintedImage(img, size = 20) {
+  const offscreen = document.createElement('canvas');
+  offscreen.width = size;
+  offscreen.height = size;
+  const offCtx = offscreen.getContext('2d');
+
+  offCtx.drawImage(img, 0, 0, size, size);
+
+  offCtx.globalCompositeOperation = 'source-in';
+  offCtx.fillStyle = '#fff';
+  offCtx.fillRect(0, 0, size, size);
+
+  return offscreen;
+}
 
 function preloadImages(callback) {
   if (techIcons.length === 0) {
@@ -32,6 +49,8 @@ function preloadImages(callback) {
   techIcons.forEach((src, index) => {
     const img = new Image();
     img.onload = () => {
+      loadedImages[index] = img;
+      whiteTintedImages[index] = createWhiteTintedImage(img);
       imagesLoaded++;
       if (imagesLoaded === techIcons.length) {
         callback();
@@ -45,21 +64,27 @@ function preloadImages(callback) {
       }
     };
     img.src = src;
-    loadedImages[index] = img;
   });
 }
 
 class Particle {
-  constructor(x, y, forceDot = false) {
+  constructor(x, y, forceDot = false, forceIconIndex = null) {
     this.x = x || Math.random() * canvas.width;
     this.y = y || Math.random() * canvas.height;
     this.vx = Math.random() - 0.5;
     this.vy = Math.random() - 0.5;
 
-    this.isIcon = !forceDot && Math.random() < 0.3 && loadedImages.length > 0;
+    if (forceIconIndex !== null && loadedImages.length > 0) {
+      this.isIcon = true;
+      this.iconIndex = forceIconIndex;
+    } else {
+      this.isIcon = !forceDot && Math.random() < 0.3 && loadedImages.length > 0;
+      if (this.isIcon) {
+        this.iconIndex = Math.floor(Math.random() * loadedImages.length);
+      }
+    }
 
     if (this.isIcon) {
-      this.iconIndex = Math.floor(Math.random() * loadedImages.length);
       this.size = 20;
     } else {
       this.radius = 2;
@@ -75,29 +100,29 @@ class Particle {
   }
 
   draw() {
-    if (this.isIcon && loadedImages[this.iconIndex]) {
-      ctx.save();
-      ctx.filter = 'brightness(0) invert(1)';
+    if (this.isIcon && whiteTintedImages[this.iconIndex]) {
       ctx.drawImage(
-        loadedImages[this.iconIndex],
+        whiteTintedImages[this.iconIndex],
         this.x - this.size / 2,
         this.y - this.size / 2,
         this.size,
         this.size
       );
-      ctx.restore();
     } else {
       ctx.beginPath();
       ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
       ctx.fillStyle = "#fff";
       ctx.fill();
-      ctx.closePath();
     }
   }
 }
 
 function initParticles() {
-  for (let i = 0; i < particleCount; i++) {
+  for (let i = 0; i < loadedImages.length && i < particleCount; i++) {
+    particles.push(new Particle(null, null, false, i));
+  }
+
+  for (let i = loadedImages.length; i < particleCount; i++) {
     particles.push(new Particle());
   }
 }
@@ -107,15 +132,15 @@ function connectParticles() {
     for (let j = i + 1; j < particles.length; j++) {
       const dx = particles[i].x - particles[j].x;
       const dy = particles[i].y - particles[j].y;
-      const distance = Math.sqrt(dx * dx + dy * dy);
+      const distanceSquared = dx * dx + dy * dy;
 
-      if (distance < maxDistance) {
+      if (distanceSquared < maxDistanceSquared) {
+        const distance = Math.sqrt(distanceSquared);
         ctx.beginPath();
         ctx.moveTo(particles[i].x, particles[i].y);
         ctx.lineTo(particles[j].x, particles[j].y);
         ctx.strokeStyle = `rgba(255, 255, 255, ${1 - distance / maxDistance})`;
         ctx.stroke();
-        ctx.closePath();
       }
     }
   }
@@ -137,6 +162,10 @@ function animate() {
 window.addEventListener("resize", () => {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
+  particles.forEach((particle) => {
+    particle.x = Math.min(particle.x, canvas.width);
+    particle.y = Math.min(particle.y, canvas.height);
+  });
 });
 
 window.addEventListener("click", (event) => {
